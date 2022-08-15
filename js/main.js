452400995edaaa19605d2ddbb0915a9b7157e80d
ephanzen@gmail.com <ephanzen4@gmail.com>
2022-08-15 11:39:28 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// alert("Belajar External File Javascript")
+
+// console.log('Halo Sayang');
+
+// String
+var cintaKu = "Ayam";
+console.log(cintaKu);
+
+// Number
+var doiKu = 1;
+console.log(doiKu);
+
+// Boolean
+var wallet = 1000;
+var hutang = 1000;
+console.log(wallet > hutang);
