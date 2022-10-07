558d412fed6ba1e3923ac116fdd8eb7e5dd22fad
ephanzen@gmail.com <ephanzen4@gmail.com>
2022-10-07 09:55:17 +0700

    Tipe Data

--- js/main.js
+++ js/main.js
@@ -3,14 +3,27 @@
 // console.log('Halo Sayang');
 
 // String
-var cintaKu = "Ayam";
+var cintaKu = "Fauzi Mempunyai doi";
 console.log(cintaKu);
 
 // Number
-var doiKu = 1;
+var doiKu = 10;
 console.log(doiKu);
 
 // Boolean
 var wallet = 1000;
 var hutang = 1000;
 console.log(wallet > hutang);
+
+// Tipe data array
+var pacarFauzi = ['Sintia', 'Yeyen', 'Putri'];
+console.log("List Pacar Fauzi " + pacarFauzi);
+
+// Tipe data object
+var fauzi = {
+    pacar: "Sintia",
+    selingkuhan: "Putri",
+    simpanan: "Mita",
+    poligami: "Dyah"
+}
+console.log(fauzi.pacar);
