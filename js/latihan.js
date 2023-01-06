9915b613174a94f8c126c60bf7763a2a67aa2131
Harsya Venturo <harsya@venturology.id>
2023-01-06 14:59:46 +0700

    Add Final Project BWA Basic JS

---
+++ js/latihan.js
@@ -0,0 +1,29 @@
+// alert("Belajar External File Javascript")
+
+// console.log('Halo Sayang');
+
+// String
+var cintaKu = "Fauzi Mempunyai doi";
+console.log(cintaKu);
+
+// Number
+var doiKu = 10;
+console.log(doiKu);
+
+// Boolean
+var wallet = 1000;
+var hutang = 1000;
+console.log(wallet > hutang);
+
+// Tipe data array
+var pacarFauzi = ['Sintia', 'Yeyen', 'Putri'];
+console.log("List Pacar Fauzi " + pacarFauzi);
+
+// Tipe data object
+var fauzi = {
+    pacar: "Sintia",
+    selingkuhan: "Putri",
+    simpanan: "Mita",
+    poligami: "Dyah"
+}
+console.log(fauzi.pacar);
--- js/main.js
+++ js/main.js
@@ -1,29 +1,52 @@
-// alert("Belajar External File Javascript")
+//* DOM Beginners
+// * Select an Element
+// var titleAnimalOne = document.getElementById('animalName3');
+// titleAnimalOne.innerHTML = "Burung Badak";
+// var buttonPrimary = document.getElementsByTagName('a');
+// buttonPrimary[2].innerHTML = "Sold Out";
+// var buttonPrimary = document.querySelectorAll('a.btn-view-details');
+// var stocks = false;
+// for (let i = 0; i < buttonPrimary.length; i++) {
+//     if (!stocks) {
+//         buttonPrimary[i].innerHTML = "Sold Out";
+//         buttonPrimary[i].classList.add('btn-danger');
+//         buttonPrimary[i].classList.remove('btn-primary');
+//         buttonPrimary[i].classList.add('disabled');
+//     }
+// }
 
-// console.log('Halo Sayang');
+var setAnimals = document.getElementById("animals");
+var i = 0;
 
-// String
-var cintaKu = "Fauzi Mempunyai doi";
-console.log(cintaKu);
+function saveAnimals() {
+  animalName = document.getElementById("name").value;
+  animalPrice = document.getElementById("price").value;
+  animalPict = document.getElementById("picture").value;
 
-// Number
-var doiKu = 10;
-console.log(doiKu);
-
-// Boolean
-var wallet = 1000;
-var hutang = 1000;
-console.log(wallet > hutang);
-
-// Tipe data array
-var pacarFauzi = ['Sintia', 'Yeyen', 'Putri'];
-console.log("List Pacar Fauzi " + pacarFauzi);
-
-// Tipe data object
-var fauzi = {
-    pacar: "Sintia",
-    selingkuhan: "Putri",
-    simpanan: "Mita",
-    poligami: "Dyah"
+  i++;
+  setAnimals.innerHTML +=
+    `<div class="col-lg-3 col-md-6 col-sm-6 col-12">
+    <img src="` +
+    animalPict +
+    `"
+        alt="" class="img-fluid h-50">
+    <h3 class="mt-3">` +
+    animalName +
+    `</h3>
+    <p>Rp` +
+    animalPrice +
+    `</p>
+    <p>
+        <a href="#" class="btn-view-details btn btn-primary w-100">
+            View Details
+        </a>
+        <a href="#" class="mt-2 btn btn-secondary w-100">
+            Buy Now
+        </a>
+    </p>
+</div>
+`;
+  animalName = document.getElementById("name").value = "";
+  animalPrice = document.getElementById("price").value = "";
+  animalPict = document.getElementById("picture").value = "";
 }
-console.log(fauzi.pacar);
